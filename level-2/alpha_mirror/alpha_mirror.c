1b464d6b0155c0dfbc117a1a3bcd9fa02784ae44
罗杰 <rogerdevworld@gmail.com>
2025-02-12 17:22:10 +0100

    alpha_mirror [OK]

--- level-2/alpha_mirror/alpha_mirror.c
+++ level-2/alpha_mirror/alpha_mirror.c
@@ -1,33 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   alpha_mirror.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 16:31:19 by galpers           #+#    #+#             */
-/*   Updated: 2022/04/06 16:41:37 by galpers          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
-	if (ac == 2)
-	{
-		while (av[1][i] != '\0')
-		{
-			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				av[1][i] = 219 - av[1][i];
-			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] = 155 - av[1][i];
-			write(1, &av[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+    if (argc == 2)
+    {
+        int i = 0;
+        while (argv[1][i])
+        {
+            if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+                argv[1][i] = ('a' + 'z') - argv[1][i];
+            else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+                argv[1][i] = ('A' + 'Z') - argv[1][i];
+            write(1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
 }
