f1cd595188e923f132fe3caa8faea351529d7350
罗杰 <rogerdevworld@gmail.com>
2025-02-12 17:38:32 +0100

    first_word [OK]

--- level-1/first_word/first_word.c
+++ level-1/first_word/first_word.c
@@ -16,7 +16,7 @@ int     main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		int     i = 0;
+		int	i = 0;
 		while (argv[1][i] && argv[1][i] == ' ' || argv[1][i] == '\t')
 			i++;
 		while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
