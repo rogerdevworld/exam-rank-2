997b726dc0566a9ed8c066880a8350ffc32c3811
罗杰 <rogerdevworld@gmail.com>
2025-02-13 14:53:05 +0100

    fprime [OK]

--- level-4/fprime/fprime.c
+++ level-4/fprime/fprime.c
@@ -1,51 +1,31 @@
-// Passed Moulinette 2019.09.01
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int		is_prime(int n)
+int main(int argc, char **argv)
 {
-	int i = 2;
-
-	while (i < n)
-	{
-		if (n % i == 0)
-			return (0);
-		++i;
-	}
-	return (1);
-}
-
-void	fprime(char *str)
-{
-	int n = atoi(str);
-	int factor = 2;
-	int first = 1;
-
-	if (n == 1)
-		printf("1");
-
-	while (factor <= n)
-	{
-		if (n % factor == 0 && is_prime(factor))
-		{
-			if (first == 1)
-				printf("%d", factor);
-			else
-				printf("*%d", factor);
-			first = 0;
-			n = n / factor;
-		}
-		else
-			++factor;
-	}
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc == 2)
-		fprime(argv[1]);
-
-	printf("\n");
-	return (0);
+    if (argc == 2)
+    {
+        int n = atoi(argv[1]);
+        int f = 2;
+        int first = 1;
+        
+        if (n == 1)
+            printf("1");
+        
+        while (n >= f)
+        {
+            if (n % f == 0)
+            {
+                if (!first)
+                    printf("*");
+                printf("%d", f);
+                n /= f;
+                first = 0;
+            }
+            else
+                f++;
+        }
+    }
+    printf("\n");
+    return 0;
 }
