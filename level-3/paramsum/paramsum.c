8a23753623c0150efc265d50d94629294568fa71
罗杰 <rogerdevworld@gmail.com>
2025-02-18 16:57:35 +0100

    paramsum [OK]

--- level-3/paramsum/paramsum.c
+++ level-3/paramsum/paramsum.c
@@ -1,24 +1,17 @@
-// Passed Moulinette 2019.09.01
-
 #include <unistd.h>
 
-void	ft_putnbr(int n)
+void    ft_putnbr(int n)
 {
-	char digit;
+    char c;
 
-	if (n >= 10)
-		ft_putnbr(n / 10);
-
-	digit = (n % 10) + '0';
-	write(1, &digit, 1);
+    if (n > 9)
+        ft_putnbr(n / 10);
+    c = (n % 10) + '0';
+    write(1, &c, 1);
 }
-
-int		main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	(void)argv;		// Silence 'unused parameter' error
-
-	ft_putnbr(argc - 1);
-	write(1, "\n", 1);
-
-	return (0);
+    int i = 0;
+    ft_putnbr(argc - 1);
+    write(1, "\n", 1);
 }
