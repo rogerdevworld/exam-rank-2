6a6a905699f5fee2df6e09a2ddc781a99f44908a
罗杰 <rogerdevworld@gmail.com>
2025-02-18 16:24:19 +0100

    pgcd [OK]

--- level-3/pgcd/pgcd.c
+++ level-3/pgcd/pgcd.c
@@ -1,28 +1,28 @@
-// Passed Moulinette 2019.09.01
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void	pgcd(int a, int b)
+int main(int argc, char **argv)
 {
-	int n = a;
-
-	while (n > 0)
-	{
-		if (a % n == 0 && b % n == 0)
-		{
-			printf("%d", n);
-			return;
-		}
-		--n;
-	}
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc == 3)
-		pgcd(atoi(argv[1]), atoi(argv[2]));
-
-	printf("\n");
-	return (0);
+    if (argc == 3)
+    {
+        int tmp, mayor, menor;
+        if (atoi(argv[1]) > atoi(argv[2]))
+        {
+            mayor = atoi(argv[1]);
+            menor = atoi(argv[2]);
+        }
+        else
+        {
+            menor = atoi(argv[1]);
+            mayor = atoi(argv[2]);
+        }
+        while (menor)
+        {
+            tmp = menor;
+            menor = mayor % menor;
+            mayor = tmp;
+        }
+        printf("%d", mayor);
+    }
+    printf("\n");
 }
