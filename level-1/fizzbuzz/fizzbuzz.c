0a23d62d38f454553c0cfcbf8ac94ee9f287c15c
罗杰 <rogerdevworld@gmail.com>
2025-02-18 15:56:21 +0100

    fizzbuzz [OK]

--- level-1/fizzbuzz/fizzbuzz.c
+++ level-1/fizzbuzz/fizzbuzz.c
@@ -1,48 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   fizzbuzz.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 10:08:45 by galpers           #+#    #+#             */
-/*   Updated: 2022/04/06 15:58:33 by galpers          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int	main(void)
+void    ft_putnbr(int n)
 {
-	int	i;
-	int k;
-	int l;
+    char c;
+    if (n > 9)
+        ft_putnbr(n / 10);
+    c = (n % 10) + '0';
+    write(1, &c, 1);
+}
 
-	i = 1;
-	while (i <= 100)
-	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			write(1, "fizzbuzz", 8);
-		else if (i % 3 == 0)
-			write(1, "fizz", 4);
-		else if (i % 5 == 0)
-			write(1, "buzz", 4);
-		else if (i > 10)
-		{
-			k = i / 10 + '0';
-			l = i % 10 + '0';
-			write (1, &k, 1);
-			write (1, &l, 1);
-		}
-		else
-		
-		{
-			k = i + '0';
-			write(1, &k, 1);
-		}
-		write(1,"\n"
-		
-		, 1);
-		i++;
-	}		
+int main(void)
+{
+    int i = 1;
+
+    while (i <= 100)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            write(1, "fizzbuzz", 8);
+        else if(i % 5 == 0)
+            write(1, "buzz", 4);
+        else if (i % 3 == 0)
+            write(1, "fizz", 4);
+        else
+            ft_putnbr(i);
+        write(1, "\n", 1);
+        i++;
+    }
 }
