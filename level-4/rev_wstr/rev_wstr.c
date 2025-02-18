de3b3535aa2bf48dc7decb03bd1e4ae555d8c1ea
罗杰 <rogerdevworld@gmail.com>
2025-02-18 17:32:19 +0100

    rev_wstr [OK]

--- level-4/rev_wstr/rev_wstr.c
+++ level-4/rev_wstr/rev_wstr.c
@@ -1,66 +1,76 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rev_wstr.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/18 14:03:03 by aarbaoui          #+#    #+#             */
-/*   Updated: 2023/01/18 14:50:08 by aarbaoui         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-void ft_putchar(char c)
+void    ft_strncpy(char *dest, char *src, int len)
 {
-	write(1, &c, 1);
+    int i = 0;
+
+    while (src[i] && i < len)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+char    **ft_split(char *str)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int count = 0;
+    while (str[i])
+    {
+        while(str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+            i++;
+        if (str[i])
+            count++;
+        while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+            i++;
+    }
+    char **split = (char **)malloc((count + 1) * sizeof(char *));
+    if (!split)
+        return (NULL);
+    i = 0;
+    while (str[i])
+    {
+        while(str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+            i++;
+        j = i;
+        while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+            i++;
+        split[k] = malloc(i - j + 1);
+        ft_strncpy(split[k++], &str[j], i - j);
+    }
+    split[k] = NULL;
+    return (split);
+
 }
 
 int ft_strlen(char *str)
 {
-	int i = 0;
-	
-	while (str[i])
-		i++;
-	return (i);
+    int len = 0;
+    while(str[len])
+        len++;
+    return (len);
 }
 
-int main(int ac, char *av[])
+int main(int argc, char **argv)
 {
-	char *tmp;
-	char *rev;
-	int len;
-
-	if (ac == 2)
-	{
-		tmp = av[1];
-		len = ft_strlen(tmp);
-		rev = NULL;
-		len--;
-		while (tmp[len])
-		{
-			if (tmp[len - 1] == ' ')
-			{
-				rev = &tmp[len];
-				while (*rev && *rev != ' ')
-				{
-					ft_putchar(*rev);
-					rev++;
-				}
-				ft_putchar(' ');
-			}
-			else if (len == 0)
-			{
-				rev = &tmp[len];
-				while (*rev && *rev != ' ')
-				{
-					ft_putchar(*rev);
-					rev++;
-				}
-			}
-			len--;
-		}
-	}
-	ft_putchar('\n');
+    if (argc == 2)
+    {
+        int i = 0;
+        char **split = ft_split(argv[1]);
+        while (split[i])
+            i++;
+        while (i--)
+        {
+            write(1, split[i], ft_strlen(split[i]));
+            if (i != 0)
+                write(1, " ", 1);
+        }
+        free(split);
+    }
+    write(1, "\n", 1);
 }
