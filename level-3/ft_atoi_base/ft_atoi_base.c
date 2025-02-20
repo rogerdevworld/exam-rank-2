703bf89e0e039faa0c9bed99154b800da524979a
罗杰 <rmarrero@cbr15s6.42barcelona.com>
2025-02-21 00:10:05 +0100

    ft_atoi_base [OK], norminette [OK]

--- level-3/ft_atoi_base/ft_atoi_base.c
+++ level-3/ft_atoi_base/ft_atoi_base.c
@@ -1,58 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   atoi_base.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/17 17:43:40 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/05/21 02:22:26 by jcluzet          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
 
-#include <stdio.h>//
-#include <stdlib.h>//
-
-int is_blank(char c)
-{
-	if (c <= 32)
-		return (1);
-	return (0);
-}
-
-int		isvalid(char c, int base)
-{
-	char digits[17] = "0123456789abcdef";
-	char digits2[17] = "0123456789ABCDEF";
-
-	while (base--)
-		if (digits[base] == c || digits2[base] == c)
-			return (1);
-	return (0);
-}
-
-int		value_of(char c)
+int	get_value(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (c - '0');
-	else if (c >= 'a' && c <= 'f')
+	if (c >= 'a' && c <= 'f')
 		return (c - 'a' + 10);
-	else if (c >= 'A' && c <= 'F')
+	if (c >= 'A' && c <= 'F')
 		return (c - 'A' + 10);
-	return (0);
+	return (-1);
 }
 
-int		ft_atoi_base(const char *str, int str_base)
+int	ft_atoi_base(const char *str, int str_base)
 {
-	int result;
-	int sign;
+	int	result;
+	int	sign;
+	int	value;
 
 	result = 0;
-	while (is_blank(*str))
+	sign = 1;
+	if (*str == '-')
+	{
+		sign = -1;
 		str++;
-	sign = (*str == '-') ? -1 : 1;
-	(*str == '-' || *str == '+') ? ++str : 0;
-	while (isvalid(*str, str_base))
-		result = result * str_base + value_of(*str++);
+	}
+	while ((value = get_value(*str)) != -1 && value < str_base)
+	{
+		result = result * str_base + value;
+		str++;
+	}
 	return (result * sign);
 }
