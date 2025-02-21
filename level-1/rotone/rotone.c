0b0931677f7635acbc8de4846320ec837651e664
罗杰 <rmarrero@cbr15s6.42barcelona.com>
2025-02-21 02:40:49 +0100

    rotone [OK], norminette [OK]

--- level-1/rotone/rotone.c
+++ level-1/rotone/rotone.c
@@ -1,33 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rotone.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 15:43:34 by galpers           #+#    #+#             */
-/*   Updated: 2022/04/06 15:50:34 by galpers          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
 	int i;
 
 	i = 0;
-	if (ac == 2)
+	if (argc == 2)
 	{
-		while (av[1][i]!= '\0')
+		while (argv[1][i] != '\0')
 		{
-			if ((av[1][i] >= 'a' && av[1][i] <= 'y') || (av[1][i] >= 'A' && av[1][i] <= 'Y'))
-				av[1][i] = av[1][i] + 1;
-			else if (av[1][i] == 'z' || av[1][i] == 'Z')
-				av[1][i] = av[1][i] - 25;
-			write(1, &av[1][i], 1);
+			if ((argv[1][i] >= 'a' && argv[1][i] <= 'y') || (argv[1][i] >= 'A'
+					&& argv[1][i] <= 'Y'))
+				argv[1][i] = argv[1][i] + 1;
+			else if (argv[1][i] == 'z' || argv[1][i] == 'Z')
+				argv[1][i] = argv[1][i] - 25;
+			write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
-	write(1, "\n",1);
+	write(1, "\n", 1);
 }
