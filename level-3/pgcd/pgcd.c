1edbd5dadddba37550621d40c6ebfb685970071b
罗杰 <rmarrero@cbr15s5.42barcelona.com>
2025-02-22 21:44:46 +0100

    keyboard.c [OK], norminette [OK]

--- level-3/pgcd/pgcd.c
+++ level-3/pgcd/pgcd.c
@@ -1,28 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc == 3)
-    {
-        int tmp, mayor, menor;
-        if (atoi(argv[1]) > atoi(argv[2]))
-        {
-            mayor = atoi(argv[1]);
-            menor = atoi(argv[2]);
-        }
-        else
-        {
-            menor = atoi(argv[1]);
-            mayor = atoi(argv[2]);
-        }
-        while (menor)
-        {
-            tmp = menor;
-            menor = mayor % menor;
-            mayor = tmp;
-        }
-        printf("%d", mayor);
-    }
-    printf("\n");
+	if (argc == 3)
+	{
+		int tmp, mayor, menor;
+		if (atoi(argv[1]) > atoi(argv[2]))
+		{
+			mayor = atoi(argv[1]);
+			menor = atoi(argv[2]);
+		}
+		else
+		{
+			menor = atoi(argv[1]);
+			mayor = atoi(argv[2]);
+		}
+		while (menor)
+		{
+			tmp = menor;
+			menor = mayor % menor;
+			mayor = tmp;
+		}
+		printf("%d", mayor);
+	}
+	printf("\n");
 }
