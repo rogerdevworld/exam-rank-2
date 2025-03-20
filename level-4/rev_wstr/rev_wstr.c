4c43baec5973098c994c0e387cbfec3bfcea472a
罗杰 <rogerdevworld@gmail.com>
2025-03-20 16:25:21 +0100

    rev_wstr/

--- level-4/rev_wstr/rev_wstr.c
+++ level-4/rev_wstr/rev_wstr.c
@@ -1,12 +1,21 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
 
-void    ft_strncpy(char *dest, char *src, int len)
+void ft_putstr(char *str)
 {
     int i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
 
-    while (src[i] && i < len)
+void ft_strcpy(char *dest, char *src, int len)
+{
+    int i = 0;
+    while (i < len)
     {
         dest[i] = src[i];
         i++;
@@ -14,63 +23,51 @@ void    ft_strncpy(char *dest, char *src, int len)
     dest[i] = '\0';
 }
 
-char    **ft_split(char *str)
+char **ft_split(char *str)
 {
     int i = 0;
     int j = 0;
     int k = 0;
     int count = 0;
+    //count words
     while (str[i])
     {
-        while(str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+        while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
             i++;
         if (str[i])
             count++;
         while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
             i++;
     }
-    char **split = (char **)malloc((count + 1) * sizeof(char *));
-    if (!split)
-        return (NULL);
     i = 0;
+    char **split = (char **)malloc((count + 1) * sizeof(char *));
     while (str[i])
     {
-        while(str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+        while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
             i++;
         j = i;
         while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
             i++;
         split[k] = malloc(i - j + 1);
-        ft_strncpy(split[k++], &str[j], i - j);
+        ft_strcpy(split[k++], &str[j], i - j);
     }
     split[k] = NULL;
     return (split);
-
 }
 
-int ft_strlen(char *str)
+int main()
 {
-    int len = 0;
-    while(str[len])
-        len++;
-    return (len);
-}
-
-int main(int argc, char **argv)
-{
-    if (argc == 2)
+    int i = 0;
+    int first = 1;
+    char **split  = ft_split("Hello World New Words Apply Yours Songs");
+    while (split[i])
+        i++;
+    while (i > 0)
     {
-        int i = 0;
-        char **split = ft_split(argv[1]);
-        while (split[i])
-            i++;
-        while (i--)
-        {
-            write(1, split[i], ft_strlen(split[i]));
-            if (i != 0)
-                write(1, " ", 1);
-        }
-        free(split);
+        ft_putstr(split[--i]);
+        if (i > 0)
+            ft_putstr(" ");
     }
-    write(1, "\n", 1);
+    //printf("$");
+    return 0;
 }
